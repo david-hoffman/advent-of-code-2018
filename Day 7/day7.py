24e0d25eac45c1cfdf786296684644213a699a11
David Hoffman <dave.p.hoffman@gmail.com>
2018-12-07 16:27:50 -0500

    Fail 2

--- Day 7/day7.py
+++ Day 7/day7.py
@@ -9,62 +9,77 @@ Copyright David Hoffman, 2018
 """
 
 import string
+import networkx as nx
 import numpy as np
-import scipy.ndimage as ndi
+import matplotlib.pyplot as plt
 
 """
 This seems to be a simple sorting algorithm. Start with alphabet in order
 Then swap characters as needed
 """
 
-# data = """Step C must be finished before step A can begin.
-# Step C must be finished before step F can begin.
-# Step A must be finished before step B can begin.
-# Step A must be finished before step D can begin.
-# Step B must be finished before step E can begin.
-# Step D must be finished before step E can begin.
-# Step F must be finished before step E can begin."""
-
-steps = np.array([char for char in string.ascii_uppercase])
+data = """Step C must be finished before step A can begin.
+Step C must be finished before step F can begin.
+Step A must be finished before step B can begin.
+Step A must be finished before step D can begin.
+Step B must be finished before step E can begin.
+Step D must be finished before step E can begin.
+Step F must be finished before step E can begin."""
 
 
 def parse(line):
-    return np.array((line[5], line[36]))
-
-
-def sort_steps(instructions, steps):
-    not_moved = np.zeros(len(steps))
-    for line in instructions:
-        first, last = parse(line)
-        first_idx = np.argwhere(steps == first).squeeze()
-        last_idx = np.argwhere(steps == last).squeeze()
-
-        if first_idx > last_idx:
-            steps[first_idx], steps[last_idx] = steps[last_idx], steps[first_idx]
-        else:
-            not_moved[np.array((first_idx, last_idx))] += 1
-
-    same = np.diff(not_moved) == 0
-    same = np.concatenate(((False,), same))
-    same[np.argwhere(same == 1) - 1] = True
-    lbls, num_lbl = ndi.label(same)
-
-    for lbl in range(1, num_lbl + 1):
-        to_sort = lbls == lbl
-        steps[to_sort] = np.sort(steps[to_sort])
-    print(not_moved)
+    return line[5], line[36]
 
 
 if __name__ == '__main__':
-    with open("input.txt", "r") as fp:
-        data = "".join(fp.readlines())
+    # with open("input.txt", "r") as fp:
+    #     data = "".join(fp.readlines())
 
     data = data.strip("\n").split("\n")
+    graph = nx.DiGraph()
 
-    print("".join(steps))
-    steps_old = np.zeros_like(steps)
-    while not np.array_equal(steps, steps_old):
-        steps_old = steps.copy()
-        sort_steps(data, steps)
-        print("".join(steps))
+    edges = sorted([parse(line) for line in data], key=lambda x: x[0])
+    graph.add_edges_from(edges)
 
+    # clean_graph = nx.transitive_reduction(graph)
+
+    ans = "".join(nx.dfs_postorder_nodes(graph.reverse()))
+    print("Answer 1:", ans)
+
+    # Build task tree
+    task_tree = dict()
+    all_steps = set()
+    for line in data:
+        parent, child = parse(line)
+        all_steps.add(parent)
+        all_steps.add(child)
+        try:
+            task_tree[parent].append(child)
+        except KeyError:
+            task_tree[parent] = [child]
+    
+    # print(task_tree, all_steps)
+
+    # Clear out last steps, i.e parent's with no children
+    steps_reversed = steps_to_process = sorted(all_steps.symmetric_difference(task_tree))[::-1]
+
+    # iterate over
+    while task_tree:
+        print("Answer 1:", "".join(steps_reversed[::-1]))
+        # remove steps that have been processed
+        for parent, children in task_tree.items():
+            for step in steps_to_process:
+                try:
+                    children.remove(step)
+                except ValueError:
+                    pass
+
+        steps_to_process = [parent for parent, children in task_tree.items() if not children]
+        steps_reversed += sorted(steps_to_process)[::-1]
+        # update task tree
+        task_tree = {parent: children for parent, children in task_tree.items() if children}
+        # print("task_tree", task_tree)
+        # print("steps_to_process", steps_to_process)
+        # print("steps_reversed", steps_reversed)
+
+    print("Answer 1:", "".join(steps_reversed[::-1]))
